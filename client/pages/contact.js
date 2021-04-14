7f95de2209363dac87b8b99a086494feb815ebe5
iCode-Cat <devranboyac8@gmail.com>
2021-04-14 12:27:57 +0300

    Contact added

--- client/pages/contact.js
+++ client/pages/contact.js
@@ -1,9 +1,36 @@
 import global from './pages.module.scss'
+import style from '../styles/contact.module.scss'
 const Contact = () => {
   
     return ( 
         <div className={global.container}>
-            <h1>Contact</h1>
+            <main className={style.main}>
+            <form>
+               <div>
+               <label htmlFor="yourname">Your Name <span>*</span></label>
+                <input type="text"/>
+               </div>
+               <div>
+               <label htmlFor="yourname">Email Address <span>*</span></label>
+                <input type="text"/>
+               </div>
+                <div className={style.subject}>
+                <label htmlFor="yourname">Subject <span>*</span></label>
+                <input type="text"/>
+                </div>
+                <div className={style.text}>
+                <label htmlFor="yourname" >Message <span>*</span></label>
+                <textarea name="message" id="" cols="30" rows="5"></textarea>
+                </div>
+                <button>Submit</button>
+                <div className={style.info}>
+                <p>Phone:  +971 55 315 2439</p>
+                <p>Email:  info@amerakkad.com</p>
+                <p>Abu Dhabi, U.A.E</p>
+            </div>
+            </form>
+        
+            </main>
         </div>
      );
 }
--- client/pages/ux-ui.js
+++ client/pages/ux-ui.js
@@ -1,9 +1,45 @@
+import style from '../styles/ui.module.scss';
 import global from './pages.module.scss'
 const Ui = () => {
   
     return ( 
-        <div className={global.container}>
-            <h1>Ui</h1>
+        <div className={`${global.container} ${style.main}`}>
+            <div className={style.wrapper}>
+            <div className={style.showreel}>
+            <img src="/assets/ui/image-copy.svg" alt="amer-al-akkad-ui/ux"/>
+            </div>
+            <div className={style.second}>
+            <div className={style.first_column}>
+            <div className={style.chance_holder}>
+           <div className={style.change}></div>
+            <div className={style.change}></div>
+            <div className={style.change}></div>
+            <div className={style.change}></div>
+            <div className={style.change}></div>
+           </div>
+           <p className={style.description}>
+Project Descriptions Project Descriptions Project Descriptions Project Descriptions Project Descriptions Project Descriptions Project Descriptions Project Descriptions 
+           </p>
+
+            </div>
+            <div className={style.second_column}>
+            <img src="/assets/ui/Video.svg" alt="amer-al-akkad-video"/>
+            </div>
+            </div>
+            <div className={style.pagination}>
+                <img src="/assets/ui/arrow1.svg" alt=""/>
+                <div className={style.pages}>
+                    <div>01</div>
+                    <div>02</div>
+                    <div>03</div>
+                    <div>04</div>
+                    <div>05</div>
+                    <div>06</div>
+                    <div>07</div>
+                </div>
+                <img src="/assets/ui/arrow2.svg" alt=""/>
+            </div>
+            </div>
         </div>
      );
 }
